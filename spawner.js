2a0b365073054e5f8f0df9e4598d775e5b49a4b7
Unknown <alfonsalfonsweb@web.de>
2018-10-09 21:53:29 +0200

    Spawner logic

--- spawner.js
+++ spawner.js
@@ -3,22 +3,22 @@ var spawner = {
     /** @param {Creep} creep **/
     run: function(spawn) {
 
-        console.log(_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.ticksToLive > 50))
+        console.log(_.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.ticksToLive > 50))
 
 
-        if(_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.ticksToLive > 50)){
+        if((_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.ticksToLive > 50).length) < 1){
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester1', {
                 memory: {role: 'harvester'}
             });
         }
 
-        if(_.filter(Game.creeps, (creep) => creep.memory.role == 'builder' && creep.ticksToLive > 50)){
+        if((_.filter(Game.creeps, (creep) => creep.memory.role == 'builder' && creep.ticksToLive > 50).length) < 2){
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Builder1', {
                 memory: {role: 'builder'}
             });
         }
 
-        if(_.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.ticksToLive > 50)){
+        if((_.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.ticksToLive > 50).length) < 2){
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Upgrader1', {
                 memory: {role: 'upgrader'}
             });
