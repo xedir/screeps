44e8867b519d6fe0ba1bfc2c054a56895d73cf8a
Unknown <alfonsalfonsweb@web.de>
2018-10-10 00:10:30 +0200

    asd

--- role.harvester.js
+++ role.harvester.js
@@ -11,10 +11,10 @@ var roleHarvester = {
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return ((structure.structureType == STRUCTURE_CONTAINER) && (structure.energy < structure.energyCapacity));
+                    return ((structure.structureType == STRUCTURE_CONTAINER) && (structure.store[RESOURCE_ENERGY] < structure.storeCapacity));
                 }
             });
-            console.log(targets)
+            console.log(targets);
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
