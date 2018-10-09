e7877cb39a7ed5f17cf0d881c31f99c3f42a52ba
Unknown <alfonsalfonsweb@web.de>
2018-10-09 21:58:03 +0200

    asd

--- spawner.js
+++ spawner.js
@@ -3,7 +3,8 @@ var spawner = {
     /** @param {Creep} creep **/
     run: function(spawn) {
 
-        console.log([] + _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.ticksToLive > 50))
+        var creeps = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.ticksToLive > 50);
+        console.log(creeps);
 
 
         if((_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.ticksToLive > 50).length) < 1){
