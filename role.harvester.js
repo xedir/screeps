60f14798430907cd53fb3c5987e8b6647d2cfc12
Unknown <alfonsalfonsweb@web.de>
2018-10-10 00:06:21 +0200

    asd

--- role.harvester.js
+++ role.harvester.js
@@ -14,6 +14,7 @@ var roleHarvester = {
                     return ((structure.structureType == STRUCTURE_CONTAINER) && (structure.energy < structure.energyCapacity))
                 }
             });
+            console.log(targets)
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
