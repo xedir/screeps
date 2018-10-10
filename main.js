a46a1c5e17b83cf36287fe20c78f5ef14cee897d
Unknown <alfonsalfonsweb@web.de>
2018-10-11 01:19:53 +0200

    asd

--- main.js
+++ main.js
@@ -32,13 +32,13 @@ module.exports.loop = function () {
 
 
     if(Game.spawns['Spawn1'].memory.init == false){
-        Game.spawns['Spawn1'].memory.quelle1 = Game.spawns['Spawn1'].room.find(FIND_SOURCES[0]).id;
+        Game.spawns['Spawn1'].memory.quelle1 = Game.spawns['Spawn1'].room.find(FIND_SOURCES)[0].id;
         Game.spawns['Spawn1'].memory.quelle2 = false;
 
-        console.log(Game.spawns['Spawn1'].room.find(FIND_SOURCES[0]).id);
+        console.log(Game.spawns['Spawn1'].room.find(FIND_SOURCES)[0].id);
 
         if(Game.spawns['Spawn1'].room.find(FIND_SOURCES).length > 1){
-            Game.spawns['Spawn1'].memory.quelle2 = Game.spawns['Spawn1'].room.find(FIND_SOURCES[1]).id;
+            Game.spawns['Spawn1'].memory.quelle2 = Game.spawns['Spawn1'].room.find(FIND_SOURCES)[1].id;
         }
         Game.spawns['Spawn1'].memory.init = true;
     }
