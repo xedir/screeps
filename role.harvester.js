06434ac21ef80a75ed3519251b38eb949758af70
Unknown <alfonsalfonsweb@web.de>
2018-10-10 02:17:24 +0200

    asd

--- role.harvester.js
+++ role.harvester.js
@@ -4,11 +4,15 @@ var roleHarvester = {
     run: function(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE && (creep.memory.role == "ha1")) {
+            if(creep.memory.role == "ha1") {
+                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE){
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            } else if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE && (creep.memory.role == "ha2")) {
-                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+                }
+            } else if(creep.memory.role == "ha2")
+                if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE)
+                {
+                    creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
         }
         else {
             var targets = creep.pos.findClosestByRange(FIND_STRUCTURES, {
