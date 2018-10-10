776290c0d896bc4973e8a34c13eccbf7aa9479b9
Unknown <alfonsalfonsweb@web.de>
2018-10-10 03:05:22 +0200

    asd

--- role.harvester.js
+++ role.harvester.js
@@ -26,7 +26,7 @@ var roleHarvester = {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
-                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                var targets = creep.pos.findInRange(FIND_CONSTRUCTION_SITES);
                 if(targets.length) {
                     if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
