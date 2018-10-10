9c7b08f734712bc28e4777e24d2e9c89dbcad032
Unknown <alfonsalfonsweb@web.de>
2018-10-10 22:15:13 +0200

    asd

--- role.carry.js
+++ role.carry.js
@@ -17,7 +17,11 @@ var roleCarry = {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return ((structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER)
-                        && ((structure.store[RESOURCE_ENERGY] < structure.storeCapacity) || structure.energy < structure.energyCapacity)
+                        && (
+                            if(structure.store[RESOURCE_ENERGY] < structure.storeCapacity == undefined) {
+                                structure.energy < structure.energyCapacity
+                            }
+                            else structure.store[RESOURCE_ENERGY] < structure.storeCapacity
                         && ( !sources[0].pos.findClosestByRange(structure)
                             || !sources[1].pos.findClosestByRange(structure))
                     )
