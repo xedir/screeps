ae59e41cfe9cf6dcbd17528a234179ee2c3a09c1
Unknown <alfonsalfonsweb@web.de>
2018-10-10 02:04:25 +0200

    2 Source Harvesting

--- role.harvester.js
+++ role.harvester.js
@@ -4,17 +4,20 @@ var roleHarvester = {
     run: function(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE && creep.harvest(sources[0] == creep.memory.source)) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            } else if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE && creep.harvest(sources[1] == creep.memory.source)) {
+                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return ((structure.structureType == STRUCTURE_CONTAINER) && (structure.store[RESOURCE_ENERGY] < structure.storeCapacity));
+                    return ((structure.structureType == STRUCTURE_CONTAINER) && (structure.store[RESOURCE_ENERGY] < structure.storeCapacity) );
                 }
             });
-            console.log(targets);
+            console.log(targets.findInRange(FIND_STRUCTURES));
+            targets.findInRange(FIND_STRUCTURES);
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
--- spawner.js
+++ spawner.js
@@ -3,11 +3,12 @@ var spawner = {
     /** @param {Creep} creep **/
     run: function(spawn) {
 
-        var harvesters = null;
+        var ha1 = null;
+        var ha2 = null;
         var builders = null;
         var upgraders = null;
 
-        harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.ticksToLive > 50);
+        ha1 = _.filter(Game.creeps, (creep) => creep.memory.role == 'ha1' && creep.ticksToLive > 50);
         builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder' && creep.ticksToLive > 50);
         upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.ticksToLive > 50);
 
@@ -18,21 +19,32 @@ var spawner = {
         var sources = Game.spawns['Spawn1'].room.find(FIND_SOURCES);
         console.log(sources[0].pos);
 
+        if(sources.length > 1){
+            ha2 = _.filter(Game.creeps, (creep) => creep.memory.role == 'ha2' && creep.ticksToLive > 50);
+        }
 
 
 
-        if(harvesters.length < 3){
-            var name = "Harvester" + Game.time.toString() ;
+        if(ha1.length < 3){
+            var name = "Harvester1 " + Game.time.toString() ;
             Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], name, {
-                memory: {role: 'harvester'}
+                memory: {role: 'ha1', source: sources[0]}
             });
-        } else if(builders.length < 3){
-            var name = "Builder" + Game.time.toString();
+        } else if (sources.length > 1){
+            if (ha2.length < 3){
+                var name = "Harvester2 " + Game.time.toString() ;
+                Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], name, {
+                    memory: {role: 'ha2', source: sources[1]}
+                });
+            }
+        }
+        else if(builders.length < 3){
+            var name = "Builder " + Game.time.toString();
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], name, {
                 memory: {role: 'builder'}
             });
         } else if(upgraders.length < 2){
-            var name = "Upgrader" + Game.time.toString();
+            var name = "Upgrader " + Game.time.toString();
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], name, {
                 memory: {role: 'upgrader'}
             });
