da394867d70df37faa9f5a9c62d618d0044fffff
Unknown <alfonsalfonsweb@web.de>
2018-10-11 14:46:51 +0200

    asd

--- role.builder.js
+++ role.builder.js
@@ -14,14 +14,13 @@ var roleBuilder = {
 
         if(creep.memory.building) {
 
-            var targets = creep.room.find(FIND_STRUCTURES, {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+                    return (structure.structureType == STRUCTURE_EXTENSION);
                 }
             });
             if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                if(creep.build(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
