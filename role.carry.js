89ee952797f89eb1c449125ea097428b312b73d3
Unknown <alfonsalfonsweb@web.de>
2018-10-11 03:11:22 +0200

    asd

--- role.carry.js
+++ role.carry.js
@@ -17,7 +17,7 @@ var roleCarry = {
                 filter: (structure) => structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.storeCapacity
             });
             if(targets.length > 0){
-                if (creep.transfer(targets[0]), RESOURCE_ENERGY == ERR_NOT_IN_RANGE) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             } else {
