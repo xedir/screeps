9434ada33fa843c85dc4d98dff836d3828a94237
Unknown <alfonsalfonsweb@web.de>
2018-10-11 02:57:18 +0200

    asd

--- role.builder.js
+++ role.builder.js
@@ -35,11 +35,9 @@ var roleBuilder = {
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return ((structure.structureType == STRUCTURE_CONTAINER) && (structure.store[RESOURCE_ENERGY] < (structure.storeCapacity+1)))
-                }
-            });
+            var targets = Game.spawns['Spawn1'].pos.findInRange(FIND_STRUCTURES, 3, {
+                filter: (structure) => structure.structureType == STRUCTURE_CONTAINER && structure.store.energy > 50
+            })
             if(targets.length > 0) {
                 if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
