add219c30105a67c45bcda98ceab85e5d11348d4
Unknown <alfonsalfonsweb@web.de>
2018-10-11 04:20:37 +0200

    asd

--- spawner.js
+++ spawner.js
@@ -33,7 +33,7 @@ var spawner = {
             Game.spawns['Spawn1'].spawnCreep([MOVE,MOVE,MOVE,WORK,CARRY,CARRY,CARRY], name, {
                 memory: {role: 'carry', carrying: false, quelle: 'quelle1'}
             });
-        else if(lebendeCarrysTwo.length < 2){
+        }else if(lebendeCarrysTwo.length < 2){
                 var name = "Carry " + Game.time.toString();
                 Game.spawns['Spawn1'].spawnCreep([MOVE,MOVE,MOVE,WORK,CARRY,CARRY,CARRY], name, {
                     memory: {role: 'carry', carrying: false, quelle: 'quelle2'}
