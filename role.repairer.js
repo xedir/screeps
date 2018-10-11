4234a2af8bf7f5d74de4aacfab916b70ba973e24
Unknown <alfonsalfonsweb@web.de>
2018-10-11 15:05:20 +0200

    as

--- role.repairer.js
+++ role.repairer.js
@@ -27,6 +27,7 @@ var roleRepairer = {
                 var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: object => object.hits < object.hitsMax
                 })
+                targets.sort((a,b) => a.hits - b.hits);
             if(targets.length > 0) {
                 if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
