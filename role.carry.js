bf9baa15d74fe6939a69de395857ce6cd2aac38b
Unknown <alfonsalfonsweb@web.de>
2018-10-11 03:30:12 +0200

    asd

--- role.carry.js
+++ role.carry.js
@@ -30,6 +30,10 @@ var roleCarry = {
                     if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                     }
+                } else {
+                    if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
                 }
             }
         }
