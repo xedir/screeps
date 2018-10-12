7cd407b528f63629cf65299b6aec794f16d3ea28
xedir <alfonsalfonsweb@web.de>
2018-10-12 19:31:53 +0200

    repairer become builder in idle

--- room.manager.js
+++ room.manager.js
@@ -1,12 +1,15 @@
 var roomManager = {
 
+    run: function(spawn) {
 
+        if (spawn.memory.status === 1) {
 
+            var pfad = PathFinder.search(spawn, Game.getObjectById(spawn.memory.quelle1).pos).path
 
+        }
+        console.log(pfad);
 
-
-
-
+    }
 };
 
 module.exports = roomManager;
