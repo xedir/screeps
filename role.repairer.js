463d28852833a36a505225d3cfcd3e1c4af30b5c
xedir <alfonsalfonsweb@web.de>
2018-10-12 05:15:39 +0200

    creeps tot ab 30 ticks rest

--- role.repairer.js
+++ role.repairer.js
@@ -24,25 +24,25 @@ var roleRepairer = {
 
             if (targets.length > 0){
                 targets.sort((a,b) => a.hits -b.hits);
-                creep.memory.repairJobId = '' + targets[0].id
+                creep.memory.repairJobId = '' + targets[0].id;
                 creep.say('Rep ' + targets[0].structureType);
             } else {
                 var targets = creep.pos.findClosestByPath(FIND_STRUCTURES,{
                     filter: object => (object.hitsMax - object.hits)  > (object.hitsMax * 0.1)
                 });
                 if(targets != null){
-                creep.memory.repairJobId = '' + targets.id
+                creep.memory.repairJobId = '' + targets.id;
                 creep.say('Rep ' + targets.structureType);
                 } else if (backup != null) {
 
                     creep.memory.repairJobId = '' + backup.id
-                } else {
-                    creep.memory.building = true;
-                    roleBuilder.run(creep);
                 }
-            }
+                }
 
-        }
+
+        } else {
+            creep.memory.building = true;
+            roleBuilder.run(creep);
 
         if(creep.memory.repairJobId !== 'voll' && creep.memory.repairJobId !== 'undefined' && creep.memory.repairing){
             var target = Game.getObjectById(creep.memory.repairJobId);
