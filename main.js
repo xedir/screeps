4ad85cc1774b7ccecf49a4cae44a8ab02c6892db
xedir <alfonsalfonsweb@web.de>
2018-10-12 05:03:50 +0200

    creeps tot ab 30 ticks rest

--- main.js
+++ main.js
@@ -84,6 +84,8 @@ module.exports.loop = function () {
                 if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+                if(creep.ticksToLive > 28)
+                    creep.say('RiP');
             }
         }
     }
