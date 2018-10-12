34f7d0331456c3f1979c81240aaa6ea4dfdd9b13
xedir <alfonsalfonsweb@web.de>
2018-10-12 03:05:09 +0200

    Repairer fix

--- role.repairer.js
+++ role.repairer.js
@@ -35,7 +35,7 @@ var roleRepairer = {
 
         if(creep.memory.repairJobId !== 'voll' && creep.memory.repairJobId !== 'undefined' && creep.memory.repairing){
             var target = Game.getObjectById(creep.memory.repairJobId);
-            console.log(Game.getObjectById(creep.memory.repairJobId))
+            console.log('test' + target)
         }
 /*
         if(creep.memory.repairing){
