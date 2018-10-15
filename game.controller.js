e137aea06c052d56e8365a055b0a2384e3465f85
xedir <alfonsalfonsweb@web.de>
2018-10-16 00:39:30 +0200

    repairer become builder in idle

--- game.controller.js
+++ game.controller.js
@@ -1,14 +1,18 @@
-var gameController = {
-
-    run: function() {
-
-        setupRooms();
 
 
+function setupRooms(){
+
+    var z=0;
+    Memory.myControlledRooms = Game.rooms;
+
+    for (var nameRaum in Game.rooms){
+
+        var nachbarn = Game.map.describeExits(nameRaum);
+
+        //Memory.myControlledRooms[z].nachbarn = Game.rooms;
+        z++;
+    }
 }
-
-
-};
 
 class GameController {
 
@@ -17,24 +21,6 @@ class GameController {
 
     }
 
-
-    setupRooms(){
-
-        var z=0;
-        Memory.myControlledRooms = Game.rooms;
-
-        for (var nameRaum in Game.rooms){
-
-            var nachbarn = Game.map.describeExits(nameRaum);
-
-            //Memory.myControlledRooms[z].nachbarn = Game.rooms;
-            z++;
-        }
-    }
-
-
-
-    
 }
 
 
